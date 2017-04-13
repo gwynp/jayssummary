aab13af217916872aea4f83d58e9ed187d7f168c
Gwyn Price <gwyn@gwynprice.com>
2017-04-13 12:02:35 -0400

    using jinja2 template

--- jayssummary.py
+++ jayssummary.py
@@ -13,29 +13,29 @@ import urllib2
 import json
 import feedparser
 import tinys3
+import yaml
+import jinja2
 
 # get the S3 keys and bucket name from the environment
-aws_access_key = os.environ.get('AWSAccessKeyId')
-aws_secret_key = os.environ.get('AWSSecretKey')
-aws_jayssummary_bucket = os.environ.get('AWSJaysSummaryBucket')
+aws_access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+aws_jayssummary_bucket = 'jaysummary.com'
 
 # set the working directory
+titledate='{dt:%A} {dt:%B} {dt.day}, {dt.year}'.format(dt=datetime.datetime.now())
 homedir='/opt/code/jayssummary'
 os.chdir(homedir)
-
-# create the html file
-# and put in the <head> stuff
-fo = open("index.html", "w")
-fi = open("head.html", "r")
-fo.write(fi.read())
-fi.close()
-
-# initialise the data we'll need
 link_count=0
 
 feeds = ["http://www.battersbox.ca/backend/geeklog.rdf",
-		"http://www.humandchuck.com/feeds/posts/default",
+		"http://www.humandchuck.com/?format=rss",
+		"http://www.torontosun.com/g00/2_d3d3LnRvcm9udG9zdW4uY29t_/TU9SRVBIRVVTMTAkaHR0cDovL3d3dy50b3JvbnRvc3VuLmNvbS9zcG9ydHMvYmx1ZWpheXMvcnNzLnhtbA%3D%3D_$/$/$/$",
+		"https://feeds.thescore.com/baseball/teams/4.rss",
+		"https://bunttothegap.com/feed/podcast/",
+		"http://www.jaysinthehouse.com/feeds/posts/default",
 		"http://www.bluebirdbanter.com/rss"]
+
+#feeds = ["http://www.bluebirdbanter.com/rss"]
 
 teams = (("mlb","tormlb"),
 		("aaa","bufaaa"),
@@ -95,17 +95,18 @@ def get_game_values(teamdir):
 			awayteam = value
 		if name == "home_team_city":
 			hometeam = value
-
-	homefirstname = tree.find('./home_probable_pitcher').attrib['first_name']
-	homesurname = tree.find('./home_probable_pitcher').attrib['last_name']
-	awayfirstname = tree.find('./away_probable_pitcher').attrib['first_name']
-	awaysurname = tree.find('./away_probable_pitcher').attrib['last_name']
-
-	if not awayfirstname:
+	try:
+		homefirstname = tree.find('./home_probable_pitcher').attrib['first_name']
+		homesurname = tree.find('./home_probable_pitcher').attrib['last_name']
+		awayfirstname = tree.find('./away_probable_pitcher').attrib['first_name']
+		awaysurname = tree.find('./away_probable_pitcher').attrib['last_name']
+	except:
 		awayfirstname = 'TBD'
-	if not homefirstname:
+		awaysurname=''
 		homefirstname = 'TBD'
+		homesurname=''
 
+	print "done for %s %s" % (hometeam, awayteam)
 	return gametime, timezone, venue, homefirstname, homesurname, awayfirstname, awaysurname, awayteam, hometeam, box
 
 #yesterdays scores
@@ -114,10 +115,8 @@ def get_game_scores(teamdir):
         # url is in this format
 	# http://mlb.mlb.com/mlb/gameday/index.jsp?gid=2015_07_26_balmlb_tbamlb_1
 	boxdate = linescore[-40:-14]
-	print boxdate
+	#print "boxdate = " + boxdate
 	box = "http:///mlb.mlb.com/mlb/gameday/index.jsp?gid=" + boxdate
-	print box
-	print linescore
 	opener = urllib2.build_opener()
 	try:
 		f = opener.open(linescore)
@@ -137,13 +136,11 @@ def get_game_scores(teamdir):
 
 # scores
 ordered_teams = collections.OrderedDict(teams)
-print >> fo, "<h5 class=\"header\">The Scores</small></h5> <ul class=\"collection with-header\">"
-
+#scores={}
+scores=[]
+link_count = 0
 for key in ordered_teams:
 	year,month,month_word,day = get_yesterday()
-	link_count = 0
-	message = ''
-	print >> fo,"<li class=\"collection-header\"><b>%s</b></li>" % levels[key]
 	league = key
 	teamabv = ordered_teams[key]
 	url = "http://gd2.mlb.com/components/game/" + key + "/year_" + year + "/month_" + month + "/day_" + day
@@ -158,21 +155,20 @@ for key in ordered_teams:
 			jaysuri = jaysuri[:-1]
 			jaysdir = url + "/" + jaysuri
 			(hometeam,awayteam,homeruns,awayruns,box) = get_game_scores(jaysdir)
+			#print "hometeam" + hometeam
 			if hometeam:
-				message += "%s %s - %s %s &nbsp;&nbsp;<a href=%s><i class=\"material-icons\">reorder</i></a><br>" % (hometeam,homeruns,awayteam,awayruns,box)
-	if link_count == 0:
-		message = "No Game Yesterday"
-	print >> fo,"<li class=\"collection-item\">%s</li>" % message
+				#print "in here"
+				level=levels[key]
+				scores.append([level,hometeam,awayteam,homeruns,awayruns,box])
+			
 
-print >> fo, "</ul>"
-
-#schedule
-print >> fo, "<h5 class=\"header\">The Schedule</small></h5> <ul class=\"collection with-header\">"
+#games
+game_count = 0
+games=[]
 for key in ordered_teams:
 	year,month,month_word,day = get_today()
-	link_count = 0
 	message = ''
-	print >> fo,"<li class=\"collection-header\"><b>%s</b></li>" % levels[key]
+	#print >> fo,"<li class=\"collection-header\"><b>%s</b></li>" % levels[key]
 	league = key
 	teamabv = ordered_teams[key]
 	url = "http://gd2.mlb.com/components/game/" + key + "/year_" + year + "/month_" + month + "/day_" + day
@@ -185,40 +181,33 @@ for key in ordered_teams:
 			link_count = link_count + 1
 			jaysuri = jaysuri[:-1]
 			jaysdir = url + "/" + jaysuri
-                        print jaysdir
+			#print "jaysdir = " + jaysdir
+			game_count = game_count + 1
+			level=levels[key]
 			(gametime, timezone, venue, homefirstname, homesurname, awayfirstname, awaysurname,awayteam,hometeam,box) = get_game_values(jaysdir)
-			message += "%s at %s at %s %s at %s &nbsp;&nbsp;<a href=%s><i class=\"material-icons\">info_outline</i></a><br>%s %s against %s %s<br>" % (awayteam, hometeam, gametime, timezone, venue, box, awayfirstname, awaysurname,homefirstname, homesurname)
-	if link_count == 0:
-		message = "No Game Today"
-	print >> fo,"<li class=\"collection-item\">%s</li>" % message
+			games.append([level,gametime, timezone, venue, homefirstname, homesurname, awayfirstname, awaysurname,awayteam,hometeam,box])
 
-print >> fo, "</ul>"
-
+			
 # get blog posts using feedparser
-# list of blogs to pull from:
-# print posts to the html file
-print >> fo, "<div class=\"page-header\"><h5>The Blogs</h5></div>"
-print >> fo, "<ul class=\"collapsible\" data-collapsible=\"accordion\">"
-
+allposts={}
 for feed in feeds:
-        print feed
+	posts={}
 	d = feedparser.parse(feed)
-	print >>fo, "<li><div class=\"collapsible-header\"><h6>%s</h6></div>" % d['feed']['title']
-        print >>fo, "<div class=\"collapsible-body\">"
 	for i in range(5):
-		post = "<h6> <a href=%s>%s</a> </h6>" % (d.entries[i]['link'],d['entries'][i]['title'])
-		post2 = post.encode('utf-8','ignore')
-		print >> fo, post2
-        print >> fo, "</div></li>"
-print >> fo, "</ul>"
+		#postlink,posttitle = (d.entries[i]['link'],d['entries'][i]['title'])
+		feedname = d['feed']['title']
+		postlink = d.entries[i]['link']
+		posttitle = d['entries'][i]['title']
+		posts[posttitle]=postlink
+	allposts[feedname]=posts
 
-#add the footer
-fi = open("foot.html", "r")
-fo.write(fi.read())
-
-# close files
-fi.close()
-fo.close()
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(["./"])) 
+template = env.get_template( "template.html") 
+result = template.render( titledate=titledate, scores=scores, games=games, posts=allposts)
+print result.encode('utf-8')
+with open("index.html", "wb") as fh:
+    fh.write(result.encode('utf-8'))
+fh.close()
 
 # copy to AWs S3 bucket
 print aws_access_key
